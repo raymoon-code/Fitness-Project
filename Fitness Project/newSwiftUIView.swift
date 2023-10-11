91a9765bd5b2b9cd5934180e3be119fd29b3f1fb
raymoon <raymoonedu@gmail.com>
2023-10-11 16:45:18 -0500

    new page

---
+++ Fitness Project/newSwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  newSwiftUIView.swift
+//  Fitness Project
+//
+//  Created by Tran Phat on 10/11/23.
+//
+
+import SwiftUI
+
+struct newSwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    newSwiftUIView()
+}
