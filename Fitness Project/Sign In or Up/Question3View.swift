ff587a931c3e98c19b7627103713dbd111f81959
Daniel <daniellonda2@gmail.com>
2023-11-29 06:11:16 -0600

    User account creation works
    tomorrow I will:
    - make profile page accessable
    - fix filtering for exercises, foods, and workouts based on who created them or if they are default
    
    after taking hilford final ofc

---
+++ Fitness Project/Sign In or Up/Question3View.swift
@@ -0,0 +1,18 @@
+//
+//  Question3View.swift
+//  Fitness Project
+//
+//  Created by user236922 on 11/29/23.
+//
+
+import SwiftUI
+
+struct Question3View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Question3View()
+}
